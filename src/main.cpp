514fafcef4c1a8d1b6d5ab1d5c523c60ac90b767
RomainLepro <tirelalangue98@laposte.net>
2023-01-18 20:21:38 +0100

    piloting servo with android

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <Arduino.h>
 #include "RADIO.h"
+#include <Servo.h>
 
 String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 bool changeLight = false;
 bool lightState = false;
 
+#define myServosLength 3
+
 RADIO radio;
+
+Servo myServos[myServosLength];//all servos
+
+//3,5,6,9,10,11
+int L_pin_servos[myServosLength] = {3,5,6};
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,6 +24,12 @@ void setup() {
   Serial.begin(115200);
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
+
+  //intiqlisqtion of servos
+  for (int i=0;i<myServosLength;i++){
+    myServos[i].attach(L_pin_servos[i]);
+    myServos[i].write(90); //to calm servos down
+  }
 }
 
 void loop() {
@@ -26,25 +40,28 @@ void loop() {
    // print the string when a newline arrives:
   //delay(100);
   //Serial.println("time is : " + String(millis()));
-  radio.quickSerial(10);
+  radio.quickSerial(1000);
+  myServos[0].write(radio.Ox/500*90 + 90);
 
 
   if (stringComplete) {
-    Serial.print("time is : ");
+    //send back data received
     Serial.print(millis());
     Serial.print(" : ");
-    Serial.println(inputString);
+    Serial.print(inputString);
+    Serial.print(inputString.substring(0,2));
+    Serial.print(" : ");
+    Serial.println(inputString.substring(3,inputString.length()-1));
+
+    myServos[1].write(inputString.substring(3,inputString.length()-1).toInt());
+
     // clear the string:
     inputString = "";
     stringComplete = false;
+    lightState = !lightState;
+    digitalWrite(LED_BUILTIN, lightState); 
   }
 
-  if(changeLight){
-    Serial.println(inputString);
-    changeLight = false;
-    lightState = int(millis()/1000)%2;
-    digitalWrite(LED_BUILTIN, lightState);   // turn the LED on (HIGH is the voltage level) 
-  }
 }
 
 
@@ -56,8 +73,7 @@ void serialEvent() {
     inputString += inChar;
     // if the incoming character is a newline, set a flag so the main loop can
     // do something about it:
-    changeLight = true;
-    if (inChar == '\n') {
+    if (inChar == '\n' || inputString.length()>10 ) {
       stringComplete = true;
     }
   }
